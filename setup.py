3ea678558eafca33c42b89e646bdc0b649ac4fa7
Erik Bergstrom <ebergstr@Eriks-MacBook-Pro.local>
2019-02-13 12:16:49 -0800

    Fixed issue with microhomologies in INDEL 83 plot

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ class move_ttf(install):
 
 
 setup(name='sigProfilerPlotting',
-		version='0.1.18',
+		version='0.1.19',
 		description='SigProfiler plotting tool',
 		url='',
 		author='Erik Bergstrom',
