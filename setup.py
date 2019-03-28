f61ea8c4574e9660c941efa704526238f9602e8c
Erik Bergstrom <ebergstr@Eriks-MacBook-Pro.local>
2019-03-28 13:50:18 -0700

    Fixed normalized y-axis for sparse samples

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ class move_ttf(install):
 
 
 setup(name='sigProfilerPlotting',
-		version='0.1.20',
+		version='0.1.24',
 		description='SigProfiler plotting tool',
 		url='',
 		author='Erik Bergstrom',
