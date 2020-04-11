dad61a071824417abb5ca3ce4b50f0a4f76ae9ea
erikbergstrom <ebergstr@Eriks-MacBook-Pro-4.local>
2020-04-11 09:47:09 -0700

    Added a 384_extended plot with several minor adjustments to the 288 plots

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 	with open('README.rst') as f:
 		return(f.read())
 
-VERSION = '1.1.3'
+VERSION = '1.1.4'
 
 def write_version_py(filename='sigProfilerPlotting/version.py'):
     # Copied from numpy setup.py
