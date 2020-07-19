655d91bb1a9d92fd1a2d8914e6644bc8f0a5f51f
Azhar <akhandek@eng.ucsd.edu>
2020-07-19 15:38:33 -0700

    Added SV plotting

--- sigProfilerPlotting/sigProfilerPlotting.py
+++ sigProfilerPlotting/sigProfilerPlotting.py
@@ -24,6 +24,140 @@ import pandas as pd
 import numpy as np
 
 
+def plotSV(matrix_path, output_path, project, plot_type="pdf", percentage=False):
+	"""Outputs a pdf containing Rearrangement signature plots
+
+	:param matrix_path: path to matrix with 32 channels as rows and samples as columns
+	:param output_path: path to output pdf file containing plots
+	:param project: name of project
+	:param plot_type: output type of plot (default:pdf)
+	:param percentage: True if y-axis is displayed as percentage of CNV events, False if displayed as counts (default:False)
+	>>> plotSV()
+
+	"""
+
+    def plot(counts, labels, sample, project, percentage):
+
+        if percentage:
+			counts = [(x/sum(counts))*100 for x in counts]
+
+        color_mapping = {'del':{'>10Mb':"deeppink", '1Mb-10Mb':"hotpink", '10-100Kb':"palevioletred", '100Kb-1Mb':"lightpink", '1-10Kb':"lavenderblush"},
+                     'tds':{'>10Mb':"saddlebrown", '1Mb-10Mb':"sienna", '10-100Kb':"peru", '100Kb-1Mb':"sandybrown", '1-10Kb':"linen"},
+                     'inv':{'>10Mb':"rebeccapurple", '1Mb-10Mb':"blueviolet", '10-100Kb':"mediumorchid", '100Kb-1Mb':"plum", '1-10Kb':"thistle"}}
+
+        alpha_dict = dict(enumerate(string.ascii_lowercase))
+        x_labels = ['1-10kb', '10-100kb', '100kb-1Mb', '1Mb-10Mb','>10Mb']
+        super_class = ['clustered', 'non-clustered']
+        sub_class = ['del', 'tds', 'inv', 'trans']
+        N=32
+        ticks = [0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 16, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 34]
+        width = 0.27
+        xticks = []
+        i = -1 #used to distinguish first bar from the rest
+        fig, ax = plt.subplots(figsize=(20,5))
+
+        for count, label in zip(counts, labels):
+            categories = label.split('_')
+            if len(categories) > 2:
+                rearrangement_class = categories[1]
+                size_class = categories[2]
+            i += 1 #position of bar
+            #print (categories)
+            if i == 0: #very first bar
+                if count >= 0:
+                    if len(categories) == 2: #clustered translocation or non-clustered translocation
+                        ax.bar(ticks[i], count, color="dimgray", edgecolor='black'); #translocation only has one color
+                    else:
+                        ax.bar(ticks[i], count, color=color_mapping[rearrangement_class][size_class], edgecolor='black');
+            else: #all the bars besides the first bar
+                if count >= 0:
+                    if len(categories) == 2: #clustered translocation or non-clustered translocation
+                        ax.bar(ticks[i], count, color="dimgray", edgecolor='black'); #translocation only has one color
+                    else:
+                        ax.bar(ticks[i], count, color=color_mapping[rearrangement_class][size_class], edgecolor='black');
+            xticks.append(ticks[i])
+        ax.set_xticks(xticks);
+        ax.set_xticklabels(x_labels * 3 + [' '] + x_labels * 3 + [' '], rotation=90);
+
+        #ADD PATCHES AND TEXT
+        patch_height = 0.2
+        patch_width = 5
+        left_edge = 0.151 #placement of left edge of patch
+        y_pos = 0.95 #placement of patch on y-axis
+        text_height = 0.96
+        patch_colors = ['maroon', 'darkorange', 'slateblue', 'green', 'maroon', 'darkorange', 'slateblue', 'green']
+        classes = sub_class + sub_class
+
+        trans = transforms.blended_transform_factory(ax.transData, ax.transAxes)
+        patch_locs = np.arange(0, 40, 5)
+        patch_locs1 = [0, 5, 10]
+        print (patch_locs)#position of patches in data coordinates
+        line_locs = [] # for recording positions of top level patches and separation lines
+
+        for i, loc in enumerate(patch_locs1): #add first 3 patches
+            ax.add_patch(plt.Rectangle((loc-0.5, 1), patch_width, patch_height, clip_on=False, facecolor=patch_colors[i], transform=trans));
+            plt.text(loc+1, 1.05, classes[i].capitalize(), fontsize=36, fontname='Times New Roman', fontweight='bold', color='white', transform=trans);
+
+        t="trans"
+        ax.add_patch(plt.Rectangle((15-0.5, 1), 3, patch_height, clip_on=False, facecolor='dimgray', transform=trans)); #patch for trans
+        plt.text(15-0.3, 1.05, t.capitalize(), fontsize=30, fontname='Times New Roman', fontweight='bold', color='white', transform=trans);
+
+        ax.axvline(x=18-0.5, color='black', linewidth=2);
+        line_locs.append(loc-0.5)
+
+        patch_locs2 = [18, 23, 28]
+        for i, loc in enumerate(patch_locs2): #add next 3 patches
+            ax.add_patch(plt.Rectangle((loc-0.5, 1), patch_width, patch_height, clip_on=False, facecolor=patch_colors[i], transform=trans));
+            plt.text(loc+1, 1.05, classes[i].capitalize(), fontsize=36, fontname='Times New Roman', fontweight='bold', color='white', transform=trans);
+
+        ax.add_patch(plt.Rectangle((33-0.5, 1), 3, patch_height, clip_on=False, facecolor='dimgray', transform=trans)); #patch for trans
+        plt.text(33-0.3, 1.05, t.capitalize(), fontsize=30, fontname='Times New Roman', fontweight='bold', color='white', transform=trans);
+
+        #manually add top level patches(het and LOH) and text inside patches
+        ax.add_patch(plt.Rectangle((-0.5, 1.2), patch_width*4, patch_height, clip_on=False, facecolor='gray', transform=trans));
+        plt.text(6.25, 1.2+.05, "Clustered", fontsize=42, fontname='Times New Roman', fontweight='bold', color='white', transform=trans);
+        ax.add_patch(plt.Rectangle((18-0.5, 1.2), (patch_width*3)+3, patch_height, clip_on=False, facecolor='black', transform=trans));
+        plt.text(23, 1.2+.05, "Non-Clustered", fontsize=42, fontname='Times New Roman', fontweight='bold', color='white', transform=trans);
+
+        ax.set_xticks(xticks);
+		ax.set_xticklabels(x_labels * 9 + hom_del_class, rotation=90, weight="bold", fontsize = 16);
+		ax.tick_params(labelleft=True, left=False, bottom=False)
+		ax.tick_params(axis='y', which='major', pad=0, labelsize=30)
+
+        #y-axis label
+        ax.set_ylabel("Percentage(%)", fontsize=24, fontname="Times New Roman", weight = 'bold', labelpad = 8)
+        ax.yaxis.labelpad = 1
+
+        #TITLE
+        plt.text(0, 0.90, sample, fontsize=20, fontname='Times New Roman', fontweight='bold', color='black', transform=trans)
+
+        plt.tight_layout()
+        plt.rcParams["font.weight"] = "bold"
+
+        pp.savefig(fig, dpi=600, bbox_inches='tight')
+
+    plt.style.use('ggplot')
+	plt.rcParams['axes.facecolor'] = 'white'
+	df = pd.read_csv(matrix_path, sep=None, engine='python') #flexible reading of tsv or csv
+    if plot_type == 'pdf':
+		pp = PdfPages(output_path + project + '_CNV_signatures' + '.pdf')
+	else: #input == counts
+		print("The only plot type supported at this time is pdf")
+
+    #each column vector in dataframe contains counts for a specific sample
+	label = df.columns[0]
+	labels = df[label]
+	samples = list(df)[1:]
+    for i, (col, sample) in enumerate(zip(df.columns[1:], samples)):
+        counts = list(df[col])
+        counts = [(x/sum(counts))*100 for x in counts]
+        assert(len(counts)) == 32
+        assert(len(labels)) == 32
+        plot(counts, labels, sample, project, percentage)
+    pp.close()
+
+
+
 def plotCNV(matrix_path, output_path, project, plot_type="pdf", percentage=False):
 	"""Outputs a pdf containing CNV signature plots
 
