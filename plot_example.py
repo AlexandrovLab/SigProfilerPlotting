7f021f4331a2dff8fd4ab8a8f07100474943db99
Azhar <akhandek@eng.ucsd.edu>
2020-08-02 13:37:17 -0700

    Modified plotting example

--- plot_example.py
+++ plot_example.py
@@ -7,7 +7,7 @@ output_path = "output/examples/samples"
 #matrix_path = ''
 #output_path = ''
 
-sigPlt.plotSBS(matrix_path + "breast_cancer_samples_example.SBS96.all", output_path,"BRCA_example", "96")
-sigPlt.plotSBS(matrix_path + "breast_cancer_samples_example.SBS192.all", output_path, "BRCA_example", "192")
+#sigPlt.plotSBS(matrix_path + "breast_cancer_samples_example.SBS96.all", output_path,"BRCA_example", "96")
+#sigPlt.plotSBS(matrix_path + "breast_cancer_samples_example.SBS192.all", output_path, "BRCA_example", "192")
 #sigPlt.plotDBS(matrix_path + "breast_cancer_samples_example.DBS78.all", output_path, "BRCA_example", "78")
 #sigPlt.plotID(matrix_path + "breast_cancer_samples_example.ID94.all", output_path,  "BRCA_example", "94")
