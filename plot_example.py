ff3272ece4a72da6f30f76b5d5811a85ded0e2d4
erikbergstrom <ebergstr@Eriks-MacBook-Pro-4.local>
2020-08-04 10:24:46 -0700

    Bug in example test

--- plot_example.py
+++ plot_example.py
@@ -6,7 +6,7 @@ output_path = "output/examples/"
 
 #matrix_path = ''
 #output_path = ''
-if not os.path.exist(output_path):
+if not os.path.exists(output_path):
 	os.makedirs(output_path)
 sigPlt.plotSBS(matrix_path + "breast_cancer_samples_example.SBS96.all", output_path,"BRCA_example", "96")
 sigPlt.plotSBS(matrix_path + "breast_cancer_samples_example.SBS192.all", output_path, "BRCA_example", "192")
