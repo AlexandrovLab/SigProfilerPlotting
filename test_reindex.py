82232f490c4065558580edcfb58ce821d3284652
TinaYang <tinayang0402@gmail.com>
2023-07-12 13:10:51 -0700

    modify pytest for re-index function

--- test_reindex.py
+++ test_reindex.py
@@ -7,17 +7,15 @@ import os
 
 
 SPP_PATH = sigPlt.__path__[0]
-SPP_SBS = os.path.join(SPP_PATH, "tests/input/SBS/")
-SPP_DBS = os.path.join(SPP_PATH, "tests/input/DBS/")
-SPP_ID = os.path.join(SPP_PATH, "tests/input/ID/")
-SPP_standard = os.path.join(SPP_PATH, "tests/standard/")
+SPP_SBS = os.path.join(SPP_PATH, "input/SBS/")
+SPP_DBS = os.path.join(SPP_PATH, "input/DBS/")
+SPP_ID = os.path.join(SPP_PATH, "input/ID/")
+SPP_standard = os.path.join(SPP_PATH, "standard/")
 
 #################
 ##### dumb ######
 #################
 # THIS SHOULD FAIL
-
-
 def test_dumb_images():
     test_path = SPP_standard + "SBS_288_plots_SBS288.pdf"
     standard_path = SPP_standard + "SBS_96_plots_SBS96.pdf"
@@ -36,8 +34,6 @@ def test_dumb_images():
 #################
 ##### SBS96 #####
 #################
-
-
 def test_SBS96_images():
     sigPlt.plotSBS(
         SPP_SBS + "ordered/example.SBS96.all", SPP_SBS + "output/", "test_ordered", "96"
@@ -82,8 +78,6 @@ def test_SBS96_unordered_images():
 ##################
 ##### SBS288 #####
 ##################
-
-
 def test_SBS288_images():
     sigPlt.plotSBS(
         SPP_SBS + "ordered/example.SBS288.all",
@@ -116,6 +110,100 @@ def test_SBS288_unordered_images():
 
     test_path = SPP_SBS + "output/SBS_288_plots_test_unordered.pdf"
     standard_path = SPP_standard + "SBS_288_plots_SBS288.pdf"
+
+    test = convert_from_path(test_path)
+    standard = convert_from_path(standard_path)
+
+    for page_num, (test, standard) in enumerate(zip(test, standard), start=1):
+        assert test.mode == standard.mode, f"Different image modes on page {page_num}."
+        assert test.size == standard.size, f"Different image sizes on page {page_num}."
+        assert (
+            test.tobytes() == standard.tobytes()
+        ), f"Images differ on page {page_num}."
+
+
+#################
+##### DBS78 #####
+#################
+def test_DBS78_images():
+    sigPlt.plotDBS(
+        SPP_DBS + "ordered/example.DBS78.all",
+        SPP_DBS + "output/",
+        "test_ordered",
+        "78",
+    )
+
+    test_path = SPP_DBS + "output/DBS_78_plots_test_ordered.pdf"
+    standard_path = SPP_standard + "DBS_78_plots_DBS78.pdf"
+
+    test = convert_from_path(test_path)
+    standard = convert_from_path(standard_path)
+
+    for page_num, (test, standard) in enumerate(zip(test, standard), start=1):
+        assert test.mode == standard.mode, f"Different image modes on page {page_num}."
+        assert test.size == standard.size, f"Different image sizes on page {page_num}."
+        assert (
+            test.tobytes() == standard.tobytes()
+        ), f"Images differ on page {page_num}."
+
+
+def test_DBS78_unordered_images():
+    sigPlt.plotDBS(
+        SPP_DBS + "unordered/example.DBS78.all",
+        SPP_DBS + "output/",
+        "test_unordered",
+        "78",
+    )
+
+    test_path = SPP_DBS + "output/DBS_78_plots_test_unordered.pdf"
+    standard_path = SPP_standard + "DBS_78_plots_DBS78.pdf"
+
+    test = convert_from_path(test_path)
+    standard = convert_from_path(standard_path)
+
+    for page_num, (test, standard) in enumerate(zip(test, standard), start=1):
+        assert test.mode == standard.mode, f"Different image modes on page {page_num}."
+        assert test.size == standard.size, f"Different image sizes on page {page_num}."
+        assert (
+            test.tobytes() == standard.tobytes()
+        ), f"Images differ on page {page_num}."
+
+
+#################
+##### ID83 #####
+#################
+def test_ID83_images():
+    sigPlt.plotID(
+        SPP_ID + "ordered/example.ID83.all",
+        SPP_ID + "output/",
+        "test_ordered",
+        "83",
+    )
+
+    test_path = SPP_ID + "output/ID_83_plots_test_ordered.pdf"
+    standard_path = SPP_standard + "ID_83_plots_ID83.pdf"
+
+    test = convert_from_path(test_path)
+    standard = convert_from_path(standard_path)
+
+    for page_num, (test, standard) in enumerate(zip(test, standard), start=1):
+        assert test.mode == standard.mode, f"Different image modes on page {page_num}."
+        assert test.size == standard.size, f"Different image sizes on page {page_num}."
+        assert (
+            test.tobytes() == standard.tobytes()
+        ), f"Images differ on page {page_num}."
+
+
+def test_ID83_unordered_images():
+    sigPlt.plotID(
+        SPP_ID + "unordered/example.ID83.all",
+        SPP_ID + "output/",
+        "test_unordered",
+        "83",
+    )
+
+    test_path = SPP_ID + "output/ID_83_plots_test_unordered.pdf"
+    standard_path = SPP_standard + "ID_83_plots_ID83.pdf"
 
     test = convert_from_path(test_path)
     standard = convert_from_path(standard_path)
